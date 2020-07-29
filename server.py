4d2dc10d5ba7464fad9188503489e3121f2a6f73
zacmiller6690 <rebel6690@gmail.com>
2020-07-29 16:04:15 +0530

    my website

---
+++ server.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+print(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('index1.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
+
+
+def write_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_csv(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something is wrong i can feel it'
+
+# @app.route('/work.html')
+# def work():
+#     return render_template('work.html')
+
+
+# @app.route('/works.html')
+# def works():
+#     return render_template('works.html')
+
+
+# @app.route('/contact.html')
+# def cont():
+#     return render_template('contact.html')
+
+
+# @app.route('/blog')
+# def blog():
+#     return 'These are my thoughts on blogs'
+
+# @app.route('/blog/2020/cats')
+# def blog2():
+#     return 'I love cats'
